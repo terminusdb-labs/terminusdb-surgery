35db46755c9987ba5034f7f00a54c6f65b54e757
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-01-05 12:07:54 +0100

    Updating surgery tool

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use clap::*;
 use std::io;
-use terminus_store::{storage::*, store::sync::SyncStore, Layer};
+use terminus_store::{
+    storage::*,
+    store::sync::{open_sync_archive_store, SyncStore, SyncStoreLayer},
+    Layer,
+};
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -21,25 +25,29 @@ enum Commands {
         label: Option<String>,
         /// The workdir to store mappings in
         #[arg(short = 's', long = "store")]
-        store: String,
+        store: Option<String>,
     },
     /// Lookup the node for an id starting at the current layer
     IdNode {
         /// The node
         id: String,
         /// Layer in which to start the lookup
-        layer: String,
+        layer: Option<String>,
         /// Label in which to start the lookup
         label: Option<String>,
         /// The workdir to store mappings in
         #[arg(short = 's', long = "store")]
-        store: String,
+        store: Option<String>,
     },
-}
-
-/// make this more clever about finding where the store is.
-fn open_store(store_path: &str) -> SyncStore {
-    open_sync_archive_store(store_path)
+    /// Node count of layer
+    NodeCount {
+        layer: Option<String>,
+        /// Label in which to start the lookup
+        label: Option<String>,
+        /// The workdir to store mappings in
+        #[arg(short = 's', long = "store")]
+        store: Option<String>,
+    },
 }
 
 fn open_layer_or_label(
@@ -47,18 +55,28 @@ fn open_layer_or_label(
     layer: Option<String>,
     label: Option<String>,
 ) -> Box<SyncStoreLayer> {
-    match (layer, label) {
+    let res = match (layer, label) {
         (None, None) => panic!("You must specify either a layer or a label"),
         (None, Some(label_name)) => store.create(&label_name).unwrap().head(),
-        (Some(layer_name), None) => store.get_layer_from_id(layer_name).unwrap(),
+        (Some(layer_name), None) => {
+            let layer = string_to_name(&layer_name).unwrap();
+            store.get_layer_from_id(layer)
+        }
         (Some(_), Some(_)) => panic!("You must specify either a layer or a label"),
     };
+    Box::new(res.unwrap().unwrap())
 }
 
-fn node_id(store: &str, layer: Option<String>, label: Option<String>, node: &str) -> String {
-    let store = open_store(store);
+fn node_id(store: &str, layer: Option<String>, label: Option<String>, node: &str) -> Option<u64> {
+    let store = open_sync_archive_store(store);
     let layer = open_layer_or_label(store, layer, label);
-    todo!()
+    layer.subject_id(node)
+}
+
+fn id_node(store: &str, layer: Option<String>, label: Option<String>, id: &str) -> Option<String> {
+    let store = open_sync_archive_store(store);
+    let layer = open_layer_or_label(store, layer, label);
+    layer.id_subject(id.parse().unwrap())
 }
 
 fn main() -> io::Result<()> {
@@ -71,12 +89,29 @@ fn main() -> io::Result<()> {
             label,
             store,
         } => {
+            let store = store.unwrap_or_else(|| ".".to_string());
             let id_for_node = node_id(&store, layer, label, &node);
-            println!("{id_for_node}");
+            match id_for_node {
+                Some(id) => println!("{id}"),
+                None => println!("None"),
+            };
             Ok(())
         }
         Commands::IdNode {
             id,
+            layer,
+            label,
+            store,
+        } => {
+            let store = store.unwrap_or_else(|| ".".to_string());
+            let id_for_node = id_node(&store, layer, label, &id);
+            match id_for_node {
+                Some(id) => println!("{id}"),
+                None => println!("None"),
+            };
+            Ok(())
+        }
+        Commands::NodeCount {
             layer,
             label,
             store,
